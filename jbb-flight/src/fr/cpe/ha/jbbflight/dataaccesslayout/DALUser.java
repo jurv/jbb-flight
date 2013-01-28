3b8a2c5ff5c92bdc8b403177287529673facfd5f
benjamin.chastanier@gmail.com <benjamin.chastanier@gmail.com@a649d77e-bf8c-7d9d-bef4-ad68597a9023>
2013-01-28 09:46:13 +0000

    Avancement DAL

--- jbb-flight/src/fr/cpe/ha/jbbflight/dataaccesslayout/DALUser.java
+++ jbb-flight/src/fr/cpe/ha/jbbflight/dataaccesslayout/DALUser.java
@@ -1,6 +1,18 @@
 package fr.cpe.ha.jbbflight.dataaccesslayout;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.PreparedQuery;
 
 import fr.cpe.ha.jbbflight.models.User;
 
@@ -17,6 +29,11 @@ public class DALUser {
 	 */
 	private DALUser instance = null;
 	
+	/**
+	 * Get datastore instance.
+	 */
+	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	
 	private DALUser(){}
 	
 	/**
@@ -30,10 +47,31 @@ public class DALUser {
 		return instance;
 	}
 	
+	/**
+	 * Get all undeleted Users from the datastore.
+	 * @return
+	 */
 	public List<User> GetAllUsers() {
 		
+		Query q = new Query("User")
+			.setFilter(new FilterPredicate("usr_is_deleted", FilterOperator.NOT_EQUAL, false));
+		
+		PreparedQuery pq = datastore.prepare(q);
+		
+		ArrayList<User> retUsers = new ArrayList<User>();
+		for(Entity user : pq.asList(FetchOptions.Builder.withDefaults())){
+			retUsers.add(new User(user));
+		}
+		
+		return retUsers;
 	}
 	
+	/**
+	 * Get the User corresponding with the identifier.
+	 * 
+	 * @param id User's Unique identifier
+	 * @return
+	 */
 	public User GetUserById(int id) {
 		
 	}
