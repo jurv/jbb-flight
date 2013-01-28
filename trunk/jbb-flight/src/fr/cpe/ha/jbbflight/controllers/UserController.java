5f0356c49687154f46399fed80e86e01169af239
jul.rouvier@gmail.com <jul.rouvier@gmail.com@a649d77e-bf8c-7d9d-bef4-ad68597a9023>
2013-01-28 13:00:09 +0000

    Suppression des trucs inutiles dans le web.xml

--- trunk/jbb-flight/src/fr/cpe/ha/jbbflight/controllers/UserController.java
+++ trunk/jbb-flight/src/fr/cpe/ha/jbbflight/controllers/UserController.java
@@ -1,6 +1,13 @@
 package fr.cpe.ha.jbbflight.controllers;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import fr.cpe.ha.jbbflight.dataaccesslayout.DALUser;
 import fr.cpe.ha.jbbflight.models.User;
@@ -9,8 +16,22 @@ import fr.cpe.ha.jbbflight.models.User;
  * Users' Controller
  * @author Julien Rouvier
  */
-public class UserController {
+@SuppressWarnings("serial")
+public class UserController extends HttpServlet {
 
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/html");
+		
+		/*RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/login.jsp");			
+		try {
+			dispatcher.forward(req,resp);
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}*/
+		
+		resp.getWriter().print("Bienvenue sur la servlet user !");
+	}
+	
 	/**
 	 * Return all users
 	 * @return List<User> All users registered in the datastore
